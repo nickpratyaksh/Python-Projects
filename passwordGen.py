2d84cd29a49690d549b779fa1bfce2da670e7887
nickpratyaksh <nickpratyaksh@gmail.com>
2023-03-08 22:07:21 +0530

    aditya's project

---
+++ passwordGen.py
@@ -0,0 +1,18 @@
+import random
+
+def generator(len):
+    password = ""
+    for i in range(0,len+1):
+        selector = random.randint(0,3)
+        if selector == 0:
+            password += chr(random.randint(65,90))
+        elif selector == 1:
+            password += chr(random.randint(97,122))
+        elif selector == 2:
+            password += str(random.randint(0,9))
+        elif selector == 3:
+            password += chr(random.randint(33,64))
+    return password
+
+len = int(input("How long do you want the password to be? "))
+print("The generated password is: ",generator(len))
